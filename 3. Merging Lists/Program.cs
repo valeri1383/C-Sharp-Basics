70e3be7cf366562276b42914bbf0e173828a47c6
valeri1383 <valeri1383@gmail.com>
2022-01-23 15:42:33 +0000

    Add files via upload

---
+++ 3. Merging Lists/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _3._Merging_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list1 = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> list2 = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> newList = new List<int>();
+            bool list1isSmallest = false;
+
+            int biggestList = 0;
+            if (list1.Count >= list2.Count)
+            {
+                biggestList = list1.Count;
+            }
+            else
+            {
+                biggestList = list2.Count;
+            }
+
+
+            for (int i = 0; i < biggestList; i++)
+            {
+                if (list1.Count > i)
+                {
+                    newList.Add(list1[i]);
+                }
+                if (list2.Count > i)
+                {
+                    newList.Add(list2[i]);
+                }
+
+            }
+            
+            Console.WriteLine(string.Join(" ", newList));
+        }
+
+        
+    }
+}
