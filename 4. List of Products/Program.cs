83ea84d94885be10b55753dd375905011c75cb49
valeri1383 <valeri1383@gmail.com>
2022-01-23 15:43:30 +0000

    Add files via upload

---
+++ 4. List of Products/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4._List_of_Products
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            List<string> productsList = new List<string>();
+
+            for (int i = 0; i < num; i++)
+            {
+                string product = Console.ReadLine();
+                productsList.Add(product);
+            }
+
+            productsList.Sort();
+
+            for (int i = 0; i < productsList.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{productsList[i]}");
+            }
+        }
+    }
+}
