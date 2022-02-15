6d0e8d98136ad4fdd516aa2b60634a5905268f2b
valeri1383 <valeri1383@gmail.com>
2022-02-15 22:35:16 +0000

    Add files via upload

---
+++ 1. Count Chars in a String Dictionarys/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._Count_Chars_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            var symbolsDict = new Dictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currentSym = text[i];
+                if (!symbolsDict.ContainsKey(currentSym) && currentSym.ToString() != " ")
+                {
+                    symbolsDict[currentSym] = 0;
+                }
+                if (currentSym.ToString() != " ")
+                {
+                    symbolsDict[currentSym] += 1;
+                }
+                
+            }
+
+            foreach (var item in symbolsDict)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
