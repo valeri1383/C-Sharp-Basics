81790d8ac82cac61ef2f5e51efe7d22b8da9acc0
valeri1383 <valeri1383@gmail.com>
2022-02-15 22:36:10 +0000

    Add files via upload

---
+++ 2. A Miner Task Dictionarys/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2._A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            var materials = new Dictionary<string, int>();
+
+            while (command != "stop")
+            {
+                string mineral = command;
+                int quantity = int.Parse(Console.ReadLine());
+                if (!materials.ContainsKey(mineral))
+                {
+                    materials.Add(mineral, 0);
+                }
+                materials[mineral] += quantity;
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var item in materials)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
