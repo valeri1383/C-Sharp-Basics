884bedda49aef7ed6846ab375e88f6646381bb30
valeri1383 <valeri1383@gmail.com>
2022-02-22 16:33:35 +0000

    Add files via upload

---
+++ 1. Reverse Strings Text Processing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1._Reverse_Strings_Text_Processing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                char[] result = command.ToCharArray();
+                Array.Reverse(result);
+                string reversed = new string(result);
+                Console.WriteLine($"{command} = {reversed}");
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
