2058acbfce13bf8b521e804032f3d7e427c545ce
valeri1383 <valeri1383@gmail.com>
2022-02-22 16:32:50 +0000

    Add files via upload

---
+++ 1.  Valid Usernames Text Processing/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _1.__Valid_Usernames_Text_Processing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = Console.ReadLine().Split(", ");
+            
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                string currentUser = text[i];
+                if (currentUser.Length > 3 && currentUser.Length <= 16)
+                {
+                    bool valid = true;
+                    foreach (var x in currentUser)
+                    {
+                        if (!(char.IsLetterOrDigit(x) || x == '-' || x == '_'))
+                        {
+                            valid = false;
+                            break;
+                        }
+                    }
+                    if (valid)
+                    {
+                        Console.WriteLine(currentUser);
+                    }
+                }
+            }
+        }
+    }
+}
