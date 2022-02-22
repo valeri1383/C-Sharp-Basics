73d352758fd7f3866e89c395c4b60d7c4e316ec7
valeri1383 <valeri1383@gmail.com>
2022-02-22 16:36:12 +0000

    Add files via upload

---
+++ 4. Text Filter Text Processing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _4._Text_Filter_Text_Processing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0;  j < bannedWords.Length;  j++)
+                {
+                    int wordLenght = bannedWords[j].Length;
+                    string censuredWord = new string('*', wordLenght);
+                    if (text.Contains(bannedWords[j]))
+                    {
+                        text = text.Replace(bannedWords[j], censuredWord);
+                    }
+                }
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
