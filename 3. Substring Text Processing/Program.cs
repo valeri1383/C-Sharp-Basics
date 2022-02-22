38659ac93388dd204c50eebaf29c67cca8b0144d
valeri1383 <valeri1383@gmail.com>
2022-02-22 16:35:33 +0000

    Add files via upload

---
+++ 3. Substring Text Processing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _3._Substring_Text_Processing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textToRemove = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            while (text.Contains(textToRemove))
+            {
+                int indexToremove = text.IndexOf(textToRemove);
+                text = text.Remove(indexToremove, textToRemove.Length);
+
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
