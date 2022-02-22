9d3f4da4b484a32dc13b54e38d4e87c7de124e18
valeri1383 <valeri1383@gmail.com>
2022-02-22 16:35:00 +0000

    Add files via upload

---
+++ 3.  Extract File Text Processing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _3.__Extract_File_Text_Processing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = Console.ReadLine().Split("\\");
+            string token = text[text.Length - 1];
+            string file = token.Split(".")[0];
+            string extention = token.Split(".")[1];
+            Console.WriteLine($"File name: {file}");
+            Console.WriteLine($"File extension: {extention}");
+        }
+    }
+}
