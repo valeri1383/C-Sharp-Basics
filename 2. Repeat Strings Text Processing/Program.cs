3377e734e59e1cd699602bca373ba23b35caa942
valeri1383 <valeri1383@gmail.com>
2022-02-22 16:34:18 +0000

    Add files via upload

---
+++ 2. Repeat Strings Text Processing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2._Repeat_Strings_Text_Processing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            StringBuilder result = new StringBuilder();
+
+            foreach (var word in words)
+            {
+                int count = word.Length;
+                for (int i = 0; i < count; i++)
+                {
+                    result.Append(word);
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
