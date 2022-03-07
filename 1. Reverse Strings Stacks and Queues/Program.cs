6cfb10834b7b55d269d1d05c1de2cf23b3c31c5a
valeri1383 <valeri1383@gmail.com>
2022-03-07 15:43:28 +0000

    Add files via upload

---
+++ 1. Reverse Strings Stacks and Queues/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._Reverse_Strings_Stacks_and_Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            var stack = new Stack<string>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                string currentChar = text[i].ToString();
+                stack.Push(currentChar);
+            }
+            Console.WriteLine(string.Join("", stack));
+            
+
+        }
+    }
+}
