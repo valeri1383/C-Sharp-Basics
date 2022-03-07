49c178b4b96b6d474e528f37e916e430d687a9f5
valeri1383 <valeri1383@gmail.com>
2022-03-07 15:46:33 +0000

    Add files via upload

---
+++ Problem 4.  Fast Food Stacks and Queues/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_4.__Fast_Food_Stacks_and_Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodQantity = int.Parse(Console.ReadLine());
+            var queue = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            int biggestOrder = int.MinValue;
+            bool completeOrders = true;
+
+            while (queue.Any())
+            {
+                int order = queue.Peek();
+                if (order > biggestOrder) biggestOrder = order;
+
+                if (foodQantity >= order)
+                {
+                    foodQantity -= order;
+                    queue.Dequeue();
+                }
+                else
+                {
+                    completeOrders = false;
+                    break;
+                }
+            }
+
+            Console.WriteLine(biggestOrder);
+            if (completeOrders) Console.WriteLine("Orders complete");
+           
+            else Console.WriteLine($"Orders left: {string.Join(" ", queue)}");
+        }
+    }
+}
