d9ead871fc49091859917aec6f15cac70c9dd4f9
valeri1383 <valeri1383@gmail.com>
2022-03-07 15:45:09 +0000

    Add files via upload

---
+++ 6. Supermarket Stacks and Queues/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6._Supermarket_Stacks_and_Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var queue = new Queue<string>();
+
+            while (input != "End")
+            {
+                if (input == "Paid")
+                {
+                    foreach (var item in queue) Console.WriteLine(item);
+                    queue.Clear();
+                }
+                else queue.Enqueue(input);
+                
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{queue.Count} people remaining.");
+        }
+    }
+}
