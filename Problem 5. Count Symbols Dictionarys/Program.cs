ed38f32a1795c008682c5c28a781cbcdb42bb496
valeri1383 <valeri1383@gmail.com>
2022-03-10 17:34:28 +0000

    Add files via upload

---
+++ Problem 5. Count Symbols Dictionarys/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_5._Count_Symbols_Dictionarys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            var dict = new SortedDictionary<char, int>();
+
+            foreach (var item in text)
+            {
+                if (!dict.ContainsKey(item))
+                {
+                    dict[item] = 0;
+                }
+                dict[item]++;
+            }
+
+            foreach (var item in dict.Keys)
+            {
+                Console.WriteLine($"{item}: {dict[item]} time/s");
+            }
+        }
+    }
+}
