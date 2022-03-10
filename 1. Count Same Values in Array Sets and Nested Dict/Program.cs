4bf6f6ea0ecb5183421ea72558dd61e496fd43a3
valeri1383 <valeri1383@gmail.com>
2022-03-10 17:29:20 +0000

    Add files via upload

---
+++ 1. Count Same Values in Array Sets and Nested Dict/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Count_Same_Values_in_Array_Sets_and_Nested_Dict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().ToArray();
+            var dict = new Dictionary<string, int>();
+
+            foreach (var item in numbers)
+            {
+                if (!dict.ContainsKey(item))
+                {
+                    dict[item] = 0;
+                }
+                dict[item]++;
+
+            }
+
+            foreach (var num in dict.Keys)
+            {
+                Console.WriteLine($"{num} - {dict[num]} times");
+            }
+        }
+    }
+}
