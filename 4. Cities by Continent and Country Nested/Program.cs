82e0313c6666eb83d5de342f4e15695bee672231
valeri1383 <valeri1383@gmail.com>
2022-03-10 17:30:38 +0000

    Add files via upload

---
+++ 4. Cities by Continent and Country Nested/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4._Cities_by_Continent_and_Country_Nested
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var dict = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var token = Console.ReadLine().Split();
+                string continent = token[0];
+                string countrie = token[1];
+                string city = token[2];
+                if (!dict.ContainsKey(continent))
+                {
+                    dict[continent] = new Dictionary<string, List<string>>();
+                }
+                if (!dict[continent].ContainsKey(countrie))
+                {
+                    dict[continent][countrie] = new List<string>();
+                }
+                dict[continent][countrie].Add(city);
+            }
+
+            foreach (var item in dict.Keys)
+            {
+                Console.WriteLine($"{item}:");
+                foreach (var el in dict[item].Keys)
+                {
+                    Console.WriteLine($"{el} -> {string.Join(", ", dict[item][el])}");
+                }
+            }
+        }
+    }
+}
