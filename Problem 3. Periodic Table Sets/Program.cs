d63d4dad7e6e2808541dc404ed5979a13a925650
valeri1383 <valeri1383@gmail.com>
2022-03-10 17:34:02 +0000

    Add files via upload

---
+++ Problem 3. Periodic Table Sets/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_3._Periodic_Table_Sets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var uniqueElements = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var elements = Console.ReadLine().Split();
+                foreach (var item in elements)
+                {
+                    uniqueElements.Add(item);
+                }
+
+            }
+            Console.WriteLine(string.Join(" ", uniqueElements));
+        }
+    }
+}
