5db8f9ce0f0e30b0ed4a06f417e9876a1bffde6c
valeri1383 <valeri1383@gmail.com>
2022-03-29 18:23:10 +0100

    Add files via upload

---
+++ DefiningClasses/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Family family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                var token = Console.ReadLine().Split();
+                var name = token[0];
+                var age = int.Parse(token[1]);
+                Person person = new Person(name, age);
+                family.AddMember(person);
+            }
+            
+
+
+            Console.WriteLine(family.GetOldestMember().Representation());
+        }
+    }
+}
