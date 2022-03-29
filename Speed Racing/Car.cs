32c0d7c7fff34d48cd7fb00ae5c4d3d851167925
valeri1383 <valeri1383@gmail.com>
2022-03-29 19:40:34 +0100

    Add files via upload

---
+++ Speed Racing/Car.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Speed_Racing
+{
+    public class Car
+    {
+        private string Model;
+        private double FuelAmount;
+        private double FuelConsumptionPerKilometer;
+        private double Travelleddistance;
+
+        public Car(string model, double fuelamount, double fuelconsumptionfor1km)
+        {
+            this.Model = model;
+            this.FuelAmount = fuelamount;
+            this.FuelConsumptionPerKilometer = fuelconsumptionfor1km;
+            this.Travelleddistance = 0;
+        }
+
+        public string model 
+        {
+            get { return this.model; }
+            set { this.model = value; }
+        }
+        public double fuelAmount
+        {
+            get { return this.fuelAmount; }
+            set { this.fuelAmount = value; }
+        }
+        public double fuelConsumptionPerKilometer
+        {
+            get { return this.fuelConsumptionPerKilometer; }
+            set { this.fuelConsumptionPerKilometer = value; }
+        }
+
+        public void Drive(string model,  double amountOfKm)
+        {
+            var fuelNeeded = amountOfKm * this.FuelConsumptionPerKilometer;
+            if (this.fuelAmount >= fuelNeeded)
+            {
+                this.fuelAmount -= fuelNeeded;
+                this.Travelleddistance += amountOfKm;
+            }
+            else
+            {
+                System.Console.WriteLine($"Insufficient fuel for the drive"); ;
+            }
+        }
+
+        public string Details() 
+        {
+            return $"{this.model} {this.fuelAmount} {this.Travelleddistance}";
+        }
+    }
+}
---
+++ Speed Racing/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Speed_Racing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var carList = new List<Car>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var token = Console.ReadLine().Split();
+                var model = token[0];
+                var fuelAmount = double.Parse(token[1]);
+                var fuelConsumptionFor1km = double.Parse(token[1]);
+
+                Car car = new Car(model, fuelAmount, fuelConsumptionFor1km);
+                carList.Add(car);
+            }
+
+            var command = Console.ReadLine();
+
+            while (command != "End")
+            {              
+                var carModel = command.Split()[1];
+                var amountOfKm = double.Parse(command.Split()[2]);
+                Car currentModel = carList.Find(x => x.model == carModel);
+                
+                currentModel.Drive(carModel, amountOfKm);
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var item in carList)
+            {
+                Console.WriteLine(item.Details());
+            }
+
+        }
+    }
+}
